40af3eeaaac5966a9a15f1d363c68e5a29125286
cxy-js <1664008234@qq.com>
2017-06-12 01:21:40 +0800

    修改路由为异步调用

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,49 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Wrapper from '../view/wrapper'
-import Shop from '../view/Shop'
-import play from '../view/play'
-import singin from '../components/singin'
-import Server from '../view/Server'
-import businesshall from '../view/homePlay/businessHall'
-import four4g from '../view/homePlay/four4g'
-import four4Gflycombo from '../view/homePlay/four4Gflycombo'
-import four4gMonthpackage from '../view/homePlay/four4gMonthpackage'
-import relieved from '../view/homePlay/relieved'
-import flowPay from '../view/homePlay/flowPay'
-import volte from '../view/homePlay/volte'
-import migu from '../view/homePlay/migu'
-import hongkong from '../view/homePlay/hongkong'
-
-import jf from '../view/jf'
-import me from '../view/Me'
-import menifo from '../view/homePlay/me-nifo'
+// import play from '../view/play'
 import error404 from '../components/error404'
-import search from '../view/shop-search'
-import deposite from '../view/homePlay/deposite'
-import payway from '../view/homePlay/payway'
-import flow from '../view/homePlay/flow'
-import myBill from '../view/homePlay/myBill'
-import packageAllowance from '../view/homePlay/packageAllowance'
-import business from '../view/homePlay/business'
-import detailedlistsearch from '../view/homePlay/detailedlistsearch'
-import flowdeposite from '../view/homePlay/flowdeposite'
-import combo from '../view/homePlay/combo'
-import packageAllowancecombolist from '../view/homePlay/packageAllowance-combolist'
-import flowlast from '../view/homePlay/flow-last'
-import flowComboChild from '../view/homePlay/flowComboChild'
+//导航
+const wrapper=reslove=>require(['.././view/Wrapper'],reslove)
+const shop=reslove=>require(['.././view/Shop'],reslove)
+const server=reslove=>require(['.././view/Server'],reslove)
+const jf=reslove=>require(['.././view/jf'],reslove)
+const me=reslove=>require(['.././view/Me'],reslove)
+//首页上方列表
+const deposite=reslove=>require(['.././view/homePlay/deposite'],reslove)
+const flow=reslove=>require(['.././view/homePlay/flow'],reslove)
 
+const myBill=reslove=>require(['.././view/homePlay/myBill'],reslove)
+const packageAllowance=reslove=>require(['.././view/homePlay/packageAllowance'],reslove)
+const business=reslove=>require(['.././view/homePlay/business'],reslove)
+const detailedlistsearch=reslove=>require(['.././view/homePlay/detailedlistsearch'],reslove)
+const flowdeposite=reslove=>require(['.././view/homePlay/flowdeposite'],reslove)
+const combo=reslove=>require(['.././view/homePlay/combo'],reslove)
+//首页上方列表下方
+const four4g=reslove=>require(['.././view/homePlay/four4g'],reslove)
+const four4Gflycombo=reslove=>require(['.././view/homePlay/four4Gflycombo'],reslove)
+const four4gMonthpackage=reslove=>require(['.././view/homePlay/four4gMonthpackage'],reslove)
+const relieved=reslove=>require(['.././view/homePlay/relieved'],reslove)
 
+const flowPay=reslove=>require(['.././view/homePlay/flowPay'],reslove)
+const volte=reslove=>require(['.././view/homePlay/volte'],reslove)
+const migu=reslove=>require(['.././view/homePlay/migu'],reslove)
+const hongkong=reslove=>require(['.././view/homePlay/hongkong'],reslove)
 
+//套餐明细
+const packageAllowancecombolist=reslove=>require(['.././view/homePlay/packageAllowance-combolist'],reslove)
+//搜索
+const search=reslove=>require(['.././view/shop-search'],reslove)
+//我的个人信息
+const menifo=reslove=>require(['.././view/homePlay/me-nifo'],reslove)
+//支付页面
+const payway=reslove=>require(['.././view/homePlay/payway'],reslove)
+//首页流量管家页面列表最后一项
+const flowlast=reslove=>require(['.././view/homePlay/flow-last'],reslove)
+//附近营业厅
+const businesshall=reslove=>require(['../view/homePlay/businessHall'],reslove)
+//登录页
+const singin=reslove=>require(['../components/singin'],reslove)
 
-
-
-import shopAd from '../view/shoplink-ad/shop-ad'
 Vue.use(Router)
 
 export default new Router({
@@ -46,54 +51,63 @@ export default new Router({
   base:__dirname,
   routes: [
     // {path:'*',redirect:'/error404'},
-    {path:'/',component:Wrapper},
-    {path:'/home',component:Wrapper},
-    {path:'/shop',component:Shop},
-    {path:'/shop/mobileshop',name:'mobileshop',component:reslove=>require(['../view/homePlay/mobile-shop'],reslove)},
-    {path:'/shop/flowdeposite',name:'flowdeposite',component:reslove=>require(['../view/homePlay/flowdeposite'],reslove)},
-
-    {path:'/Server',component:Server},
-    {path:'/Server/queryiphone',name:'queryiphone',component:reslove=>require(['../view/queryiphone'],reslove)},
-    {path:'/businesshall',component:businesshall},
-    {path:'/four4g',component:four4g},
-    {path:'/four4Gflycombo',component:four4Gflycombo},
-    {path:'/four4gMonthpackage',component:four4gMonthpackage},
-    {path:'/relieved',component:relieved},
-    {path:'/flowPay',component:flowPay},
-    {path:'/volte',component:volte},
-    {path:'/migu',component:migu},
-    {path:'/hongkong',component:hongkong},
-
-    {path:'/singin',component:singin},
-
-
+    {path:'/',component:wrapper},
+    {path:'/home',component:wrapper},
+    {path:'/shop',component:shop},
+    {path:'/Server',component:server},
     {path:'/jf',component:jf},
     {path:'/me',component:me},
-    {path:'/menifo',component:menifo},
-    //公用搜索
-    {path:'/search',component:search},
-    //首页上方列表路由
+    //首页充值缴费
     {path:'/deposite',component:deposite},
     {path:'/deposite/payway',name:'payway',component:reslove=>require(['../view/homePlay/payway'],reslove)},
+     //首页流量管家
     {path:'/flow',component:flow},
-
+    //首页流量管家页面列表最后一项
+    {path:'/flowlast',component:flowlast},
+    //我的账单
     {path:'/mybill',component:myBill},
+    //套餐余量
     {path:'/packageallowance',component:packageAllowance},
     {path:'/packageallowance/liuliang',name:'liuliang',component:reslove=>require(['../view/homePlay/packageAllowance-combolist'],reslove)},
+    //业务分类
     {path:'/business',component:business},
+    //详单查询
     {path:'/detailedlistsearch',component:detailedlistsearch},
+    //流量充值
     {path:'/flowdeposite',component:flowdeposite},
+    //套餐订购
     {path:'/combo',component:combo},
+    //4G全攻略
+    {path:'/four4g',component:four4g},
+    //4G飞享套餐
+    {path:'/four4Gflycombo',component:four4Gflycombo},
+    //4G流量月包
+    {path:'/four4gMonthpackage',component:four4gMonthpackage},
+    //4G流量安心包
+    {path:'/relieved',component:relieved},
+    //4G流量统付
+    {path:'/flowPay',component:flowPay},
+    //VOLTE
+    {path:'/volte',component:volte},
+    //咪咕
+    {path:'/migu',component:migu},
+    //国际港澳台
+    {path:'/hongkong',component:hongkong},
+    //更多热卖机型
+    {path:'/shop/mobileshop',name:'mobileshop',component:reslove=>require(['../view/homePlay/mobile-shop'],reslove)},
+    {path:'/shop/flowdeposite',name:'flowdeposite',component:reslove=>require(['../view/homePlay/flowdeposite'],reslove)},
+    //套餐明细
     {path:'/packageAllowancecombolist',component:packageAllowancecombolist},
-    {path:'/flowlast',component:flowlast},
-    {path:'/flowComboChild',component:flowComboChild},
-
-    //商城
-    {path:'/shopAd',component:shopAd},
-
-
-
-
-
+    //搜索
+    {path:'/search',component:search},
+    //我的个人信息
+    {path:'/menifo',component:menifo},
+    //附近营业厅
+    {path:'/businesshall',component:businesshall},
+    //登录
+    {path:'/singin',component:singin},
+    //号码归属地
+    {path:'/Server/queryiphone',name:'queryiphone',component:reslove=>require(['../view/queryiphone'],reslove)},
+    
   ]
 })
